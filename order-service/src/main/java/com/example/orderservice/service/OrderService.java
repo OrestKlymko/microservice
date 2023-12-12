95a7de4b0009a44c6bff5edc483f672aa88dc181
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-12-12 10:46:25 +0100

    create kafka logic

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,35 +1,58 @@
 package com.example.orderservice.service;
 
 
+import com.example.orderservice.dto.InventoryResponse;
 import com.example.orderservice.dto.OrderLineItemsDto;
 import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.event.OrderPlacedEvent;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.model.OrderLineItems;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 @Transactional
 public class OrderService {
 
 	private final OrderRepository orderRepository;
+	private final WebClient webClient;
+	private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
-	public void placeOrder(OrderRequest orderRequest){
+	public void placeOrder(OrderRequest orderRequest) {
 		List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList().stream().map(this::mapToDo).toList();
-		Order orderMap = Order.builder()
+		Order order = Order.builder()
 				.orderNumber(UUID.randomUUID().toString())
 				.orderLineItemsList(orderLineItemsList)
 				.build();
-		orderRepository.save(orderMap);
-		log.info("Order saved");
+
+		List<String> skuCodesList = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuOrder).toList();
+
+
+		InventoryResponse[] inventoryResponseArray = webClient.get()
+				.uri("http://localhost:8084/api/inventory",
+						uriBuilder -> uriBuilder.queryParam("skuCode", skuCodesList).build())
+				.retrieve()
+				.bodyToMono(InventoryResponse[].class)
+				.block();
+
+
+		if (inventoryResponseArray == null) throw new IllegalStateException("inventoryResponseArray is null");
+		boolean allProductInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+		if (allProductInStock) {
+			orderRepository.save(order);
+			kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+		} else {
+			throw new IllegalArgumentException("Product is not at stock");
+		}
 
 	}
 
